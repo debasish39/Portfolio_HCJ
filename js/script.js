462153fd2ab0e95b0a42bbff3bcab4ce44ae0bfc
Debasish Panda <156610494+debasish39@users.noreply.github.com>
2026-01-30 14:10:01 +0530

    Changed

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ menuToggle.addEventListener("click", () => {
 });
 
 // Close menu after clicking a link
-navItems.forEach(link => {
+navItems.forEach((link) => {
   link.addEventListener("click", () => {
     menuToggle.classList.remove("active");
     navLinks.classList.remove("active");
@@ -17,35 +17,35 @@ navItems.forEach(link => {
 });
 
 window.addEventListener("scroll", () => {
- if(window.scrollY>300){
-     scrollTopBtn.style.display="flex";
- }
- else{
-    scrollTopBtn.style.display="none";
- }});
- scrollTopBtn.addEventListener("click", () => {
+  if (window.scrollY > 300) {
+    scrollTopBtn.style.display = "flex";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+});
+scrollTopBtn.addEventListener("click", () => {
   window.scrollTo({
     top: 0,
-    behavior: "smooth"
+    behavior: "smooth",
   });
 });
 
-const form=document.getElementById("contactForm");
-form.addEventListener("submit",async(e)=>{
+const form = document.getElementById("contactForm");
+form.addEventListener("submit", async (e) => {
   e.preventDefault();
-  const formData=new FormData(form);
+  const formData = new FormData(form);
   const response = await fetch("https://api.web3forms.com/submit", {
     method: "POST",
-    body: formData
+    body: formData,
   });
-const result=await response.json();
-console.log(result);
-if(result.success){
-  alert(`Thank you, ${result.data.name}, for reaching out. we will get back to you soon.`);
+  const result = await response.json();
+  console.log(result);
+  if (result.success) {
+    alert(
+      `Thank you, ${result.data.name}, for reaching out. we will get back to you soon.`,
+    );
     form.reset();
-}
-else{
-      alert(`Something went wrong! ${result.message}`);
-
-}
-})
+  } else {
+    alert(`Something went wrong! ${result.message}`);
+  }
+});
